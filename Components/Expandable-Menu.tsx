4c344df3f90ac00d9720c1f622e890a80c6f9d95
Abhay0294 <abhaypratap349@gmail.com>
2022-06-18 23:51:27 +0530

    Created welcome page for the basic react app

--- Components/Expandable-Menu.tsx
+++ Components/Expandable-Menu.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
+import '../style';
 
 export default function ExpandSideBars() {
-  
-  function printInfo(): string {
-    alert('Hello Abhay');
-    return 'Hello Abhay';
-  }
-
-  return <div>{printInfo()}</div>;
+  return (
+    <div className="expandable-panel">
+      <ul>
+        <li>LeetCode</li>
+        <hr className="hr-line" />
+        <li>Geeks4Geeks</li>
+        <hr className="hr-line" />
+        <li>HackerRank</li>
+        <hr className="hr-line" />
+        <li>HackerEarth</li>
+        <hr className="hr-line" />
+        <li>Codality</li>
+        <hr className="hr-line" />
+      </ul>
+    </div>
+  );
 }
--- Components/Welcome-Page.tsx
+++ Components/Welcome-Page.tsx
@@ -4,22 +4,52 @@ import '@fortawesome/fontawesome-free/js/all.js';
 import ExpandSideBars from './Expandable-Menu';
 
 export class Welcome extends React.Component {
+  state = {
+    expand: false,
+  };
+
   render() {
     return (
       <div className="main-container">
         <h1 className="header">Namaste React</h1>
         <div className="header-panel">
           <div className="button-container">
-            <div className="fa-solid fa-house-chimney" title="Home"></div>
-            <div className="fa-solid fa-code" title="Code"></div>
-            <div className="fa-solid fa-book" title="Documentation"></div>
+            <button>
+              <i className="fa-solid fa-house-chimney" title="Home"></i>
+            </button>
+            <button>
+              <i className="fa-solid fa-code" title="Code"></i>
+            </button>
+            <button>
+              <i className="fa-solid fa-book" title="Documentation"></i>
+            </button>
           </div>
-          <div
-            className="fa-solid fa-bars"
-            onClick={() => <ExpandSideBars />}
-          ></div>
+          <button onClick={this.changeState}>
+            <i className="fa-solid fa-bars"></i>
+          </button>
+        </div>
+        <div className="expand-bars">
+          {this.state.expand && <ExpandSideBars />}
+        </div>
+        <div className="empty-space"></div>
+        <div className="techno-section">
+          <div className="p-language">JavaScript</div>
+          <div className="p-language">React</div>
+          <div className="p-language">HTML</div>
+          <div className="p-language">CSS</div>
         </div>
       </div>
     );
   }
+
+  changeState = () => {
+    if (this.state.expand) {
+      this.setState({
+        expand: false,
+      });
+    } else
+      this.setState({
+        expand: true,
+      });
+  };
 }
